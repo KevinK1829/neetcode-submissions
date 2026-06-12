e93498f5106b845030f287ff2840b32f5b3734b6
KevinK1829 <74681110+KevinK1829@users.noreply.github.com>
2026-06-12 12:57:11 -0500

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #Step 1: make result HashMap but it has to be defaultdict(list) so we can append 
+        #key values starting with an empty list
+        #Step 2: loop through words and create count array of size 26
+        #Step 3: loop through each letter in the word and add counts using ASCII
+        #Step 4: append words to correct count using tuple since keys need to be static
+        #Step 5: return list of values from HashMap
+
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26 #a..z
+            for c in s:
+                #ASCII Language
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+        
+        return list(res.values())
+
+
+        
+        
+        
